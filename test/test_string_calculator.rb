d3c8d4cb251773985b93d172d8d21d3c8c756fc7
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2025-01-14 00:00:42 +0530

    Test two comma separated numbers

--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -14,4 +14,9 @@ class TestStringCalculator < Minitest::Test
     assert_equal 1, @calculator.add("1")
     assert_equal 2, @calculator.add("2")
   end
+
+  def test_two_comma_separated_numbers
+    assert_equal 6, @calculator.add("1,5")
+    assert_equal 7, @calculator.add("3,4")
+  end
 end
