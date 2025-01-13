4ef35578f38413fc103fd95284c14e6a56ed5561
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2025-01-13 23:41:03 +0530

    Test single string number

--- test/test_string_calculator.rb
+++ test/test_string_calculator.rb
@@ -9,4 +9,9 @@ class TestStringCalculator < Minitest::Test
   def test_empty_string_returns_zero
     assert_equal 0, @calculator.add("")
   end
+
+  def test_single_string_number
+    assert_equal 1, @calculator.add("1")
+    assert_equal 2, @calculator.add("2")
+  end
 end
