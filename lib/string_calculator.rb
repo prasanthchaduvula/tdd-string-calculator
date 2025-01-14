57a9c49d6cb7ebceadfb486ab4d55a9b0d4f732c
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2025-01-14 08:52:31 +0530

    Implement support for different delimters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
   def add(numbers_str)
     return 0 if numbers_str.empty?
 
-    numbers_str.split(/,|\n/).reduce(0) { |total, element| total + element.to_i }
+    match_numbers = /\d+/
+
+    numbers_str.scan(match_numbers).reduce(0) { |total, element| total + element.to_i }
   end
 end
