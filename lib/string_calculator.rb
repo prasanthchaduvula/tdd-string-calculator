aee8d248b6e5beb5d065c077efc2fdde49f0b7c1
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2025-01-14 08:40:22 +0530

    Enhance add method to handle newline as delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers_str)
     return 0 if numbers_str.empty?
 
-    numbers_str.split(",").reduce(0) { |total, element| total + element.to_i }
+    numbers_str.split(/,|\n/).reduce(0) { |total, element| total + element.to_i }
   end
 end
